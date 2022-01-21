df46b4d8f2f2bd902ae87513c5d59b53a5ef98f1
0x464e <36742501+0x464e@users.noreply.github.com>
2022-01-21 20:13:39 +0200

    Median exercise

--- 03/src/Median.java
+++ 03/src/Median.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Median {
+import java.util.Arrays;
+
+public class Median
+{
+    public static void main(String[] args)
+    {
+        var inputArray = Arrays.stream(args).mapToDouble(Double::parseDouble).sorted().toArray();
+        System.out.println("Median: " + (inputArray.length % 2 == 0 ? (inputArray[inputArray.length/2 - 1] + inputArray[inputArray.length/2])/2 : inputArray[inputArray.length/2]));
+    }
 }

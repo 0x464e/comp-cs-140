a73080860e4eada3e805ffaeb7a87f911247dc5f
0x464e <36742501+0x464e@users.noreply.github.com>
2022-02-09 02:25:15 +0200

    Cleanup

--- 02/src/Mean.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class Mean
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Mean: " + Arrays.stream(Arrays.stream(args).mapToDouble(Double::parseDouble).toArray()).average().getAsDouble());
-    }
-}

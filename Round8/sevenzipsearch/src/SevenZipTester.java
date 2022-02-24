8cb9ab9c96b725693a81da27651153a1389d2a2f
0x464e <36742501+0x464e@users.noreply.github.com>
2022-02-24 21:39:52 +0200

    Add 7z exercise

---
+++ Round8/sevenzipsearch/src/SevenZipTester.java
@@ -0,0 +1,42 @@
+import org.apache.commons.compress.archivers.sevenz.SevenZFile;
+
+import java.io.*;
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+public class SevenZipTester
+{
+    public static void main(String[] args) throws IOException
+    {
+        var fileName = args[0];
+        var searchWord = args[1];
+
+        try (var sevenZFile = new SevenZFile(new File(fileName)))
+        {
+            var entries = sevenZFile.getEntries();
+            for (var entry : entries)
+            {
+                var name = entry.getName();
+                if (!name.matches(".+?\\.txt$"))
+                    continue;
+
+                System.out.format("%s%n", name);
+
+                try (var reader = new BufferedReader(new StringReader(new String(sevenZFile.getInputStream(entry).readAllBytes()))))
+                {
+                    String line;
+                    var i = 1;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        var matcher = Pattern.compile("(" + searchWord + ")", Pattern.CASE_INSENSITIVE).matcher(line);
+                        if (matcher.find())
+                            System.out.format("%d: %s%n", i, matcher.replaceAll(x -> x.group().toUpperCase(Locale.ROOT)));
+
+                        i++;
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
